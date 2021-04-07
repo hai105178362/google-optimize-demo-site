cf7b8adf5438089eaeebccfd50bbbe98d1d7b74d
hai105178362 <48752239+hai105178362@users.noreply.github.com>
2021-04-07 15:34:41 +0800

    Create index.js

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-window.onload = function() {
-  document.getElementById('main-content').innerHTML = 'Hello, github pages :)'
-}
+
 
 function myFunction() {
   document.getElementById("demo").innerHTML = "Hello World";
