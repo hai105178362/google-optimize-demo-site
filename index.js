bd039d87cf29466d8b248bd3361a797caa48913f
hai105178362 <48752239+hai105178362@users.noreply.github.com>
2021-04-07 14:37:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 window.onload = function() {
   document.getElementById('main-content').innerHTML = 'Hello, github pages :)'
 }
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Hello World";
+}
